23fd1cafb778dd7f14480806b1c6972df1ba8c68
Muhamed Abdo <mohamedabdo9999933@gmail.com>
2026-02-06 20:38:18 +0200

    feat: update azkar icons with custom assets and refine UI/data

--- lib/data/wakeup_azkar.dart
+++ lib/data/wakeup_azkar.dart
@@ -1,8 +1,25 @@
 import 'package:imsakia/models/azkar.dart';
 
-final List<Azkar>wakeupAzkarList=[
-  Azkar(id: 'wakeup_1',
-   category: 'wakeup', 
-   text: 'الحَمْـدُ لِلّهِ الّذي أَحْـيانا بَعْـدَ ما أَماتَـنا وَإليه النُّـشور.',
- target: 1)
+final List<Azkar> wakeupAzkarList = [
+  Azkar(
+    id: 'wakeup_1',
+    category: 'wakeup',
+    text:
+        'الحَمْـدُ لِلّهِ الّذي أَحْـيانا بَعْـدَ ما أَماتَـنا وَإليه النُّـشور.',
+    target: 1,
+  ),
+  Azkar(
+    id: 'wakeup_2',
+    category: 'wakeup',
+    text:
+        'الحمدُ للهِ الذي عافاني في جَسَدي وَرَدّ عَليّ روحي وَأَذِنَ لي بِذِكْرِه',
+    target: 1,
+  ),
+  Azkar(
+    id: 'wakeup_3',
+    category: 'wakeup',
+    text:
+        'لا إلهَ إلاّ اللّهُ وَحْـدَهُ لا شَـريكَ له، لهُ المُلـكُ ولهُ الحَمـد، وهوَ على كلّ شيءٍ قدير، سُـبْحانَ اللهِ، والحمْـدُ لله ، ولا إلهَ إلاّ اللهُ واللهُ أكبَر، وَلا حَولَ وَلا قوّة إلاّ باللّهِ العليّ العظيم. رَبِّ اغْفرْ لي. ',
+    target: 1,
+  ),
 ];
